fff419e5907efbca757c44181ee3e8e7f1598045
Thai Nguyen <developers@buscoro.com>
2020-10-23 14:15:11 -0500

    first commit

---
+++ js/add_contact.js
@@ -0,0 +1,93 @@
+function addContact() {
+  const firstName = $("#firstName")[0].value;
+  const lastName = $("#lastName")[0].value;
+  const phone = $("#phone")[0].value;
+  const address = $("#address")[0].value;
+
+  const readyToSubmit = validateInput(firstName, lastName, phone, address);
+
+  if (readyToSubmit && confirm("Do you want to create a new contact?")) {
+    // if successfully submit clear box and display notification
+    alert("Create a new contact successfully!");
+
+    $("input").val("");
+    $(".notify").remove();
+    $(".require").remove();
+
+    displayContact(firstName, lastName, phone, address);
+  }
+}
+
+// field validation & notify
+function validateInput(firstName, lastName, phone, address) {
+  let completed = true;
+
+  $(".notify").remove();
+
+  if (firstName.length == 0) {
+    completed = false;
+    $("label.firstName").append("<span class='notify'><b> *</b></span>");
+    required();
+  }
+  if (firstName.length >= 50 || lastName.length >= 50) {
+    completed = false;
+    $("#add").after(
+      "<span class='notify'><br /><b>*</b><i> Name has to be less than 50 characters</i></span>"
+    );
+  }
+  if (firstName.match(/[^a-zA-Z\s]/) || lastName.match(/[^a-zA-Z]/)) {
+    completed = false;
+    $("#add").after(
+      "<span class='notify'><br /><b>*</b><i> Name can only contain letters and spaces</i></span>"
+    );
+  }
+  if (phone.length == 0) {
+    completed = false;
+    $("label.phone").append("<span class='notify'><b> *</b></span>");
+    required();
+  }
+  if (phone.length <= 5 || phone.length >= 15) {
+    completed = false;
+    $("#add").after(
+      "<span class='notify'><br /><b>*</b><i> Phone number has to be more than 5 characters and less than 15 characters</i></span>"
+    );
+  }
+  if (address.length == 0) {
+    completed = false;
+    $("label.address").append("<span class='notify'><b> *</b></span>");
+    required();
+  }
+  if (address.length >= 100) {
+    completed = false;
+    $("#add").after(
+      "<span class='notify'><br /><b>*</b><i> Address has to be less than 100 characters</i></span>"
+    );
+  }
+  if (address.match(/[^a-zA-Z0-9,\s]/)) {
+    completed = false;
+    $("#add").after(
+      "<span class='notify'><br /><b>*</b><i> Name can only contain letters, numbers, commas, and spaces</i></span>"
+    );
+  }
+
+  return completed;
+}
+
+function required() {
+  $(".require").remove();
+  $("#add").after(
+    "<span class='require'><br /><b>*</b><i> Required</i></span>"
+  );
+}
+
+// add new contact to the contact book
+function displayContact(firstName, lastName, phone, address) {
+  const contact = `
+    <div class="card">
+      <h4 class="name">${firstName.concat(" ", lastName)}</h4>
+      <p class="phone">${phone}</p>
+      <p class="address">${address}</p>
+    </div>`;
+
+  $("#contact").after(contact);
+}
---
+++ js/search.js
@@ -0,0 +1,56 @@
+function search() {
+  const query = $("#name")[0].value;
+  const nameArray = $("h4.name");
+  const phoneArray = $("p.phone");
+  const addressArray = $("p.address");
+
+  // search is case insensitive
+  const queryLowerCase = query.toLowerCase();
+  const nameArrayLowerCase = $.map(nameArray, (x) => x.innerHTML.toLowerCase());
+
+  const indexArray = searchByRecursion(nameArrayLowerCase, queryLowerCase, []);
+
+  // show the number of result
+  $(".notify").remove();
+  const message = `<span class='notify'><br />${indexArray.length} result(s) found.</span>`;
+  $("#search").after(message);
+
+  // display matching contact
+  $(".searchResult").remove();
+  indexArray.forEach((x) => {
+    displayResult(
+      nameArray[x].innerHTML,
+      phoneArray[x].innerHTML,
+      addressArray[x].innerHTML
+    );
+  });
+}
+
+function searchByRecursion(array, query, indexArray) {
+  // if the query is none return the indexArray of all elements
+  if (query.length == 0) {
+    return [...array.keys()];
+  }
+
+  // partial search instead of an exact search that can be done much simpler with a hash
+  const matchedIndex = array.findIndex((x) => x.includes(query) == true);
+  if (matchedIndex != -1) {
+    indexArray.push(matchedIndex);
+    array[matchedIndex] = "";
+    return searchByRecursion(array, query, indexArray);
+  } else {
+    return indexArray;
+  }
+}
+
+// display search result
+function displayResult(name, phone, address) {
+  const contact = `
+    <div class="searchResult">
+      <h4 class="searchName">${name}</h4>
+      <p class="searchPhone">${phone}</p>
+      <p class="searchAddress">${address}</p>
+    </div>`;
+
+  $("#search").after(contact);
+}
